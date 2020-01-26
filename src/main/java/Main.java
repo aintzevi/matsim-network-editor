aa859202b05d58590ab8548066623731fe4d3d25
aintzevi <intz.katerina@gmail.com>
2020-01-26 18:20:05 +0100

    Create basic javafx application main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,30 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.net.URL;
+
+public class Main extends Application {
+    Stage mainWindow;
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        launch(args);
     }
+
+    @Override
+    public void start(Stage mainStage) throws Exception {
+        // Changed from Parent root = FXMLLoader.load(getClass().getResource("menu.fxml"));
+        // This fixes no Location found exception
+        mainWindow = mainStage;
+
+        URL url = new File("src/main/resources/fxml/menu.fxml").toURI().toURL();
+        Parent root = FXMLLoader.load(url);
+        mainWindow.setTitle("MATSim network editor");
+        mainWindow.setScene(new Scene(root, 800, 650));
+        mainWindow.show();
+     }
+
+     //TODO init() and/or stop()?
 }
