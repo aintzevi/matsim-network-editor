824af399359f52fdf2c10cf095776c2716cd57e8
aintzevi <intz.katerina@gmail.com>
2020-04-16 23:39:56 +0200

    Add Storage Service for map caching

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,30 +1,88 @@
+import com.gluonhq.charm.down.ServiceFactory;
+import com.gluonhq.charm.down.Services;
+import com.gluonhq.charm.down.plugins.StorageService;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
+
+import static com.sun.javafx.util.Utils.*;
+
+//import static com.sun.javafx.PlatformUtil.*;
 
 public class Main extends Application {
-    Stage mainWindow;
     public static void main(String[] args) {
+
+        if(isWindows() || isMac() || isUnix()) {
+            System.setProperty("javafx.platform" , "Desktop");
+        }
+
+        // define service for desktop - to cache the map
+        StorageService storageService = new StorageService() {
+            @Override
+            public Optional<File> getPrivateStorage() {
+                // user home app config location (linux: /home/[yourname]/.gluonmaps)
+                return Optional.of(new File(System.getProperty("user.home")));
+            }
+
+            @Override
+            public Optional<File> getPublicStorage(String subdirectory) {
+                // this should work on desktop systems because home path is public
+                return getPrivateStorage();
+            }
+
+            @Override
+            public boolean isExternalStorageWritable() {
+                return getPrivateStorage().get().canWrite();
+            }
+
+            @Override
+            public boolean isExternalStorageReadable() {
+                return getPrivateStorage().get().canRead();
+            }
+        };
+
+        ServiceFactory<StorageService> storageServiceFactory = new ServiceFactory<StorageService>() {
+
+            @Override
+            public Class<StorageService> getServiceType() {
+                return StorageService.class;
+            }
+
+            @Override
+            public Optional<StorageService> getInstance() {
+                return Optional.of(storageService);
+            }
+
+        };
+
+        // register service
+        Services.registerServiceFactory(storageServiceFactory);
+
         launch(args);
     }
 
     @Override
-    public void start(Stage mainStage) throws Exception {
-        // Changed from Parent root = FXMLLoader.load(getClass().getResource("menu.fxml"));
+    public void start(Stage mainStage) throws IOException {
+        // Changed from Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("menu.fxml"));
         // This fixes no Location found exception
-        mainWindow = mainStage;
-
-        URL url = new File("src/main/resources/fxml/menu.fxml").toURI().toURL();
+        URL url = new File("src/main/resources/fxml/main.fxml").toURI().toURL();
         Parent root = FXMLLoader.load(url);
-        mainWindow.setTitle("MATSim network editor");
-        mainWindow.setScene(new Scene(root, 800, 650));
-        mainWindow.show();
-     }
 
-     //TODO init() and/or stop()?
+        GridPane nodesGridPane = new GridPane();
+        nodesGridPane.setId("nodesGridPane");
+
+        mainStage.setTitle("MATSim network editor");
+        mainStage.setScene(new Scene(root, 800, 650));
+        mainStage.show();
+    }
+
+    //TODO init() and/or stop()?
 }
